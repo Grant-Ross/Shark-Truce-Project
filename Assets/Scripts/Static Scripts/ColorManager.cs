7c329dd30f1def764e80715b7087f01d4a1d4c82
Grant Ross <36680394+Grant-Ross@users.noreply.github.com>
2021-09-26 10:43:07 -0500

    level 1 assets

--- Assets/Scripts/Static Scripts/ColorManager.cs
+++ Assets/Scripts/Static Scripts/ColorManager.cs
@@ -9,6 +9,8 @@ public static class ColorManager
         Red, Blue, Green
     }
 
-    private static readonly Color[] colors = {Color.red, Color.blue, Color.green};
+    private static readonly Color[] colors = {new Color(248/255f,80/255f,120/255f), 
+        new Color(141/255f,141/255f,248/255f), 
+        new Color(100/255f,248/255f,100/255f)};
     public static Color GetHue(GameColor color){return colors[(int) color];}
 }
